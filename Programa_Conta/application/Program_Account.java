0f8dcad8aeb45ee7a3ccf6aa7f44ca90c0bfef15
Caio Bahia <caiocarlosbahia@gmail.com>
2020-09-20 17:01:34 -0300

    Mensagem explicativa e comentario teste

--- Programa_Conta/application/Program_Account.java
+++ Programa_Conta/application/Program_Account.java
@@ -13,13 +13,14 @@ public class Program_Account {
 		Scanner sc = new Scanner(System.in);
 		Account account = null;
 			
+		//Teste feito para o Github
 		System.out.print("Enter account number: ");
 		int numberAccount = sc.nextInt();
 		System.out.print("Enter account holder: ");
 		sc.nextLine();
 		String holder = sc.nextLine();
 		
-		System.out.print("Is there na initial deposit (y/n)?");
+		System.out.print("Is there na initial deposit (y/n) ?");
 		char initial = sc.next().charAt(0);
 		if (initial == 'y' || initial == 'Y' ) {
 			System.out.print("Enter initial deposit value: ");
